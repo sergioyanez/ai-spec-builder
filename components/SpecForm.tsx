8fd51a9eda70130c7f279aabbe8ade2221a7d88c
Sergio Yañez <sergiomyanez01@gmail.com>
2026-07-04 13:20:05 -0300

    Se expande la capacidad del textarea

--- components/SpecForm.tsx
+++ components/SpecForm.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import type { Spec } from "@/lib/types";
 
-const MAX_CHARS = 500;
+const MAX_CHARS = 1000;
 
 interface SpecFormProps {
   onResult: (spec: Spec) => void;
